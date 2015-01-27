2c2b6cd2e6d6202c4ef99cbe45bb99694a85f649
karlTH <karl_trimix@hotmail.com>
2015-01-27 22:33:52 +0100

    class guino for arduino

---
+++ src/guino/tool/Guino.java
@@ -0,0 +1,41 @@
+package guino.tool;
+
+import java.io.File;
+import java.io.IOException;
+
+import processing.app.Editor;
+import processing.app.tools.Tool;
+
+public class Guino implements Tool {
+
+	public String getMenuTitle() {
+		return "Guino";
+	}
+
+	public void init(Editor arg0) {
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		Runtime rt = Runtime.getRuntime();
+		
+		try {
+			
+			String dir = getClass().getClassLoader().getResource(".").getFile();
+			
+			Process ps = rt.exec(dir + "..\\tools\\Guino\\tool\\guino\\GUINO.exe", null, new File(dir + "..\\tools\\Guino\\tool\\guino"));
+			  ps.waitFor();
+		      
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
