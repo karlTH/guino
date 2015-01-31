92fe4a5938b891ee19d349d2162c8aff3f6c5518
karlTH <karl_trimix@hotmail.com>
2015-01-31 17:28:36 +0100

    bug with space in directory

--- src/guino/tool/Guino.java
+++ src/guino/tool/Guino.java
@@ -25,8 +25,8 @@ public class Guino implements Tool {
 		try {
 			
 			String dir = getClass().getClassLoader().getResource(".").getFile();
-			
-			Process ps = rt.exec(dir + "..\\tools\\Guino\\tool\\guino\\GUINO.exe", null, new File(dir + "..\\tools\\Guino\\tool\\guino"));
+						
+			 Process ps = rt.exec(dir.replaceAll("%20", " ") + "..\\tools\\Guino\\tool\\guino\\GUINO.exe", null, new File(dir.replaceAll("%20", " ") + "..\\tools\\Guino\\tool\\guino"));
 			  ps.waitFor();
 		      
 		} catch (IOException e) {
